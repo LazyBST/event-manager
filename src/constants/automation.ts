1cf95a26adda17314af22209e7f4e38aef1ab6fa
Ayush Arora <ayusharora1778@gmail.com>
2024-04-03 14:25:34 +0530

    chore: updated automation trigger export

---
+++ src/constants/automation.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export enum AUTOMATION_TRIGGER {
+  'app-cp-created' = 'app-cp-created',
+  'dashboard-cp-created' = 'dashboard-cp-created',
+  'app-cp-edited' = 'app-cp-edited',
+  'dashboard-cp-edited' = 'dashboard-cp-edited',
+}
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -13,11 +13,3 @@ export const eventTypeTopicMap = {
   [EventType.AUTOMATION_DLQ_EVENT.toString()]:
     AUTOMATION_DLQ_TOPIC || AUTOMATION_DLQ_DEFAULT_TOPIC,
 }
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
-export enum AUTOMATION_TRIGGER {
-  'app-cp-created' = 'app-cp-created',
-  'dashboard-cp-created' = 'dashboard-cp-created',
-  'app-cp-edited' = 'app-cp-edited',
-  'dashboard-cp-edited' = 'dashboard-cp-edited',
-}
