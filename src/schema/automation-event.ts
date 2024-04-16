1a59a793c6731848719b4b2a6dd793e85ad4a528
Ayush Arora <ayusharora1778@gmail.com>
2024-04-16 15:00:55 +0530

    fix: removed unwanted interfaces

--- src/schema/automation-event.ts
+++ src/schema/automation-event.ts
@@ -1,14 +1,6 @@
 import { Type } from '@sinclair/typebox'
 import { AUTOMATION_TRIGGER } from '../constants'
 
-const IHeaders = Type.Object({
-  authToken: Type.String(),
-})
-
-const IRequestContext = Type.Object({
-  headers: IHeaders,
-})
-
 const IUserOtherDetials = Type.Object({
   custom: Type.Record(
     Type.String(),
@@ -27,7 +19,6 @@ const IClusterUser = Type.Object({
 })
 
 export const AutomationEventSchema = Type.Object({
-  ctx: IRequestContext,
   user: IClusterUser,
   trigger: Type.Enum(AUTOMATION_TRIGGER),
   companyCode: Type.String(),
